28926ad60b18454882debdcb56d1337d33270a24
Tran Dai Nguyen <nguyentd1004@gmail.com>
2018-09-28 18:27:35 +0700

    Add HealthSystem, remove unnecessary files

---
+++ Assets/_Character/HealthSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] Image healthBar;
+    [SerializeField] AudioClip[] damageSounds;
+    [SerializeField] AudioClip[] deathSounds;
+    [SerializeField] float deadVanishAfter = 2f;
+
+    const string DEATH_TRIGGER = "Death";
+    Animator animator;
+    AudioSource audioSource;
+    Character character;
+
+    float currentHealthPoints;
+
+    public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        character = GetComponent<Character>();
+
+        SetCurrentMaxHealth();
+    }
+
+    private void SetCurrentMaxHealth()
+    {
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBar)//Enemies may not have health bar to update
+        {
+            healthBar.fillAmount = healthAsPercentage;
+        }
+    }
+    //TODO Impliment TakeDamage()
+
+    //TODO Impliment KillCharacter()
+}
---
+++ Assets/_Character/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CameraRaycaster cameraRaycaster;
+
+    Character character;
+
+    void Start()
+    {
+        character = GetComponent<Character>();
+
+        RegisterForMouseEvents();
+    }
+
+    void RegisterForMouseEvents()
+    {
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+        cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+        cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOverPotentiallyWalkable(Vector3 destination)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            character.SetDestination(destination);
+        }
+    }
+
+    void OnMouseOverEnemy(Enemy enemy)
+    {
+        //TODO Impliment
+    }
+}
