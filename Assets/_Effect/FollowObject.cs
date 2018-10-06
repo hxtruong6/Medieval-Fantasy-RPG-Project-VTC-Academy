215df4f8eb7b7edfa871889a14ce8fc089e0d9a2
Quang <phunghuuduyquang@gmail.com>
2018-10-06 21:11:01 +0700

    Update lv1

---
+++ Assets/_Effect/FollowObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    public Transform playerTransform;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
+    public bool lootAtPlayer = true;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = playerTransform.position;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        if (lootAtPlayer)
+        {
+            transform.LookAt(playerTransform);
+        }
+    }
+}
