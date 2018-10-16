c6ca0991eb4eb640f938e4a095d10fc35e5b77b6
Quang <phunghuuduyquang@gmail.com>
2018-10-16 18:49:09 +0700

    add enemy and lobby

---
+++ Assets/_Camera UI/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    CameraRaycaster cameraRaycaster;
+
+    Character character;
+
+    void Start()
+    {
+        character = GetComponent<Character>();
+
+        RegisterForMouseEvents();
+    }
+
+    void RegisterForMouseEvents()
+    {
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+        cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseOverPotentiallyWalkable(Vector3 destination)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            character.SetDestination(destination);
+        }
+    }
+}
