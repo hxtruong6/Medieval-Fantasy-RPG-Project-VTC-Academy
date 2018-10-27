7da88e46df1f5c45790fa907153e875bf0bc841f
hxtruong <hoangxuantruong98@gmail.com>
2018-10-27 19:52:49 +0700

    switch weapon in area spawns

--- Assets/_Character/Enemies/Skeleton/SkeletonArea.cs
+++ Assets/_Character/Enemies/Skeleton/SkeletonArea.cs
@@ -1,17 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SkeletonArea : MonoBehaviour
 {
 
     // Use this for initialization
     //public float spawnRadius = 8f;
-    public GameObject skeleton;
+    public GameObject skeletonPrefab;
     public GameObject[] listOfSpawn;
     public int numberSpawn = 1;
+    public int selectedWeapon = 0;
+
+    public WeaponConfig[] listOfWeapon;
+
+    [ExecuteInEditMode]
+    void OnValidate()
+    {
+        selectedWeapon = Mathf.Clamp(selectedWeapon, 0, listOfWeapon.Length - 1);
+    }
 
     void Start()
     {
-        //listOfSpawn = gameObject.GetComponentsInParent<GameObject>();
     }
 
     // Update is called once per frame
@@ -28,11 +37,35 @@ public class SkeletonArea : MonoBehaviour
             {
                 for (int i = 0; i < listOfSpawn.Length; i++)
                 {
-                    var skeletonPrefab = Instantiate(skeleton);
-                    skeletonPrefab.transform.position = listOfSpawn[i].transform.position;
-                    skeletonPrefab.transform.SetParent(this.transform);
+                    GameObject skeletonClone = Instantiate(skeletonPrefab);
+                    skeletonClone.transform.position = listOfSpawn[i].transform.position;
+                    skeletonClone.transform.SetParent(this.transform);
+                    var animatorDummny = skeletonClone.GetComponentInChildren<Animator>();
+                    StartCoroutine(spawnWeapon(skeletonClone, animatorDummny.runtimeAnimatorController.animationClips[0].length));
+                    //setSkeletonWeapon(skeletonPrefab);
                 }
             }
         }
     }
+
+    IEnumerator spawnWeapon(GameObject skeletonClone, float clipLength)
+    {
+        yield return new WaitForSeconds(clipLength);
+        var weapon = skeletonClone.GetComponentInChildren<WeaponSystem>();
+        weapon.PutWeaponInHand(listOfWeapon[selectedWeapon]);
+    }
+
+    //private void setSkeletonWeapon(GameObject skeletonPrefab)
+    //{
+    //    var gameObjs = skeletonPrefab.GetComponentsInChildren<WeaponSystem>();
+    //    for (int i = 0; i < gameObjs.Length; i++)
+    //    {
+    //        if (gameObjs[i] is WeaponSystem)
+    //        {
+    //            //WeaponSystem weapon = gameObjs[i] as WeaponSystem;
+    //            //weapon.SetCurrentWeapon(listOfWeapon[selectedWeapon]);
+    //        }
+    //    }
+
+    //}
 }
---
+++ Assets/_Character/Enemies/Skeleton/SkeletonWeapon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SkeletonWeapon : MonoBehaviour
+{
+    
+  
+    void Start()
+    {
+        //Assert.IsFalse(listOfWeapon.Length == 0, "Them Weapon cho Skeleton");
+       
+    }
+}
